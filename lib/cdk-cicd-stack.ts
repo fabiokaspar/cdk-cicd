b05d3d2c3cde2fe5f0b090dc5a47128d86d179c2
root <root@Fabio.localdomain>
2026-03-04 16:33:13 -0300

    add some fixes

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,24 @@
 import * as cdk from 'aws-cdk-lib/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
+  private readonly repoName: string = 'fabiokaspar/cdk-cicd'
+  private readonly repoBranch: string = 'development'
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'MyPipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(this.repoName, this.repoBranch),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+    })
   }
 }
